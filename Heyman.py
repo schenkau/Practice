ddf91e99c53f8eebcba4ee02f5b3d17f3730a4a0
Austin Schenk <austinschenkbyu@gmail.com>
2017-02-16 22:11:51 -0700

    Added stuff in branch only

--- Heyman.py
+++ Heyman.py
@@ -12,3 +12,6 @@ THis was done on the COMBINING branch
 now I have to see if I can get it over to the main branch 
 since it works
 
+NOw I did this in the text editor and I just want to see if it happens in the branch
+or if it happens on the master
+I did it in the branch
